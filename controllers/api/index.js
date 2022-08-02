b874920474f2ea2736ebc8cdd6f5bb06b37bae05
Marina H <mxhuisken@gmail.com>
2022-08-01 21:24:55 -0700

    more route work

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const postRoutes = require('./postRoutes');
+const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/post', postRoutes);
+router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+//HOMEPAGE AND DASHBOARD ARE LIKE VIEW ROUTES 
+//API ROUTES MORE LIKE BACKEND THAT USER DOES NOT SEE FOR SENDING AND RECEIVING DATA
+
+//route for seeing all posts
+//route for seeing single post 
+
+
+//route login
+
+module.exports = router;
